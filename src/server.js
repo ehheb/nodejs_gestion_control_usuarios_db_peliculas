8ddf6a4b229751f753e9526192ad944c185124f0
hebert <hebertvillarreal1@gmail.com>
2021-02-18 17:06:58 -0700

    Se crea el archivo auth dentro de la carpeta routes y se enlaza al archivo server.js

--- src/server.js
+++ src/server.js
@@ -1,10 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
+import authRouter from "./routes/auth";
 dotenv.config();
 
 const PORT = process.env.PORT || 8000;
 const app = express();
+
 app.use(express.json());
+app.use(authRouter);
 
 app.listen(PORT, () => {
     console.log(`Corriendo sobre el puerto ${PORT}`);
