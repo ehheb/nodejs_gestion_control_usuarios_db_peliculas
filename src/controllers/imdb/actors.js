64ff05b846e3b08ffe90b1f49d74835df8c3c29a
hebert <hebertvillarreal1@gmail.com>
2021-03-04 20:26:28 -0700

    Se realizan todos los métodos de actores, solamente falta agregar el método de eliminar algun actor, también faltan realizar las validaciones en cada uno de estos métodos

--- src/controllers/imdb/actors.js
+++ src/controllers/imdb/actors.js
@@ -3,11 +3,11 @@ import {Actors} from "../../models";
 export const getAllActors = async(req, res) => {
     try {
         let results = await Actors.findAll();
-        res.status(200).json({
+        return res.status(200).json({
             results
         });
     } catch(error) {
-        res.status(500).json({
+        return res.status(500).json({
             message: "Error al obtener a los actores"
         });
     }
@@ -17,12 +17,12 @@ export const getActorById = async(req, res) => {
     try {
         let id = req.params.id;
         let results = await Actors.findOne({ where: {id: id}});
-        res.status(201).json({
+        return res.status(201).json({
             results
         });
 
     } catch(error) {
-        res.status(500).json({
+        return res.status(500).json({
             message: "Error al obtener actor",
             error
         });
@@ -34,15 +34,48 @@ export const postActor = async(req, res) => {
         let name = req.body.name;
 
         let results = await Actors.create({name: name});
-        res.status(201).json({
+        return res.status(201).json({
             message: "Se ha añadido a un nuevo actor de manera satisfactoria",
             results
         })
 
     } catch(error) {
-        res.status(500).json({
+        return res.status(500).json({
             message: "Error al crear nuevo actor",
             error
         });
     }
+}
+
+export const updateActor = async(req, res) => {
+    let id = req.params.id;
+    let renameActor = req.body.name;
+    try {
+        let findActor = await Actors.findOne({where: {id: id}});
+    if(findActor) {
+        if(renameActor) {
+            let results = await Actors.update({name: renameActor}, { where: {id: id}});
+            console.log(findActor.id);
+            return res.status(201).json({
+                message: "Se ha actualizado el actor",
+                de: findActor.name,
+                por: renameActor
+            });
+        } else {
+            return res.status(401).json({
+                message: "Debe de colocar dentro del cuerpo el nuevo nombre"
+            })
+        }
+    } else {
+        return res.status(401).json({
+            message: "El id del actor no se encontró"
+        })
+    } 
+    } catch(error) {
+        return res.status(500).json({
+            message: "Error al encontrar el actor",
+            error
+        })
+    }
+
 }
--- src/routes/actors.js
+++ src/routes/actors.js
@@ -1,10 +1,11 @@
 import express from "express";
-import {getAllActors, getActorById, postActor} from "../controllers/imdb/actors";
+import {getAllActors, getActorById, postActor, updateActor} from "../controllers/imdb/actors";
 
 const router = express.Router();
 
 router.get("/actors", getAllActors);
 router.get("/actors/:id", getActorById);
 router.post("/actors", postActor);
+router.put("/actors/:id", updateActor);
 
 export default router;
