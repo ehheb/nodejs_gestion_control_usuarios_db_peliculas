344c0fb300a4e327c7d1bd925ce85f5bf4acaec5
hebert <hebertvillarreal1@gmail.com>
2021-03-09 17:52:53 -0700

    Se realiza el método put para la tabla de contenidos, solamente falta realizar el método delete y solamente faltan realizar los métodos get, post put y delete para la tabla episodelists y las tablas pivotes

--- src/controllers/imdb/contents.js
+++ src/controllers/imdb/contents.js
@@ -111,3 +111,78 @@ export const createContent = async (req, res) => {
         });
     }
 }
+
+export const updateContent = async (req, res) => {
+    let id = req.params.id;
+
+    let title = req.body.title;
+    let description = req.body.description;
+    let totalSeasons = req.body.totalSeasons;
+    let imdbScore = req.body.imdbScore;
+    let releaseDates = req.body.releaseDates;
+    let playTime = req.body.playTime;
+    let contentRatingId = req.body.contentRatingId;
+    let totalEpisodes = req.body.totalEpisodes;
+    let contentTypeId = req.body.contentTypeId;
+    let imdbLink = req.body.imdbLink;
+    let imdbScoreVotes = req.body.imdbScoreVotes;
+    let ratingDetails = req.body.ratingDetails;
+    let languages = req.body.languages;
+    
+    try {
+        let findContentId = await Contents.findOne({where: {id: id}});
+
+        if(findContentId) {
+            let contentRating = await ContentRatings.findOne({where: {id: contentRatingId}});
+
+            if(contentRating) {
+                let contentType = await ContentTypes.findOne({where: {id: contentTypeId}});
+    
+                if(contentType) {
+                    await Contents.update({
+                        title: title,
+                        description: description,
+                        totalSeasons: totalSeasons,
+                        imdbScore: imdbScore,
+                        releaseDates: releaseDates,
+                        playTime: playTime,
+                        contentRatingId: contentRatingId,
+                        totalEpisodes, totalEpisodes,
+                        contentTypeId: contentTypeId,
+                        imdbLink: imdbLink,
+                        imdbScoreVotes: imdbScoreVotes,
+                        ratingDetails: ratingDetails,
+                        languages: languages
+                    }, {where: {id: id}});
+                    const newUpdateContent = await Contents.findOne({where: {id: id}})
+
+                        return res.status(201).json({
+                            message: "El contenido se actualizó de manera éxitosa",
+                            newUpdateContent
+                        });
+
+                    
+                } else {
+                    return res.status(401).json({
+                        message: "El id del tipo de contenido es inválido"
+                    });
+                }
+    
+            } else {
+                return res.status(401).json({
+                    message: "El id del rating de contenido es inválido"
+                });
+            }
+
+        } else {
+            return res.status(401).json({
+                message: "El id del contenido no se encontró"
+            });
+        }
+
+    } catch(error) {
+        return res.status(500).json({
+            message: "Error al actualizar contenido"
+        });
+    }
+}
--- src/routes/imdb/contents.js
+++ src/routes/imdb/contents.js
@@ -1,12 +1,12 @@
 import express from "express";
-import {getAllContents, getContentById, createContent} from "../../controllers/imdb/contents";
+import {getAllContents, getContentById, createContent, updateContent} from "../../controllers/imdb/contents";
 import {validateFindInUrl, findInUrl, validateContent, createContentSchema} from "../../middlewares/validators";
 
 const router = express.Router();
 
-
 router.get("/contents", getAllContents);
 router.get("/contents/:id", validateFindInUrl(findInUrl), getContentById);
 router.post("/contents", validateContent(createContentSchema), createContent);
+router.put("/contents/:id", validateFindInUrl(findInUrl), validateContent(createContentSchema), updateContent);
 
 export default router;
